19be4796e3ee5ae2c9d0926f5829338b58ef05a5
Bryan Quach <bryancquach@gmail.com>
2020-03-09 20:09:22 +0100

    updated merge_salmon_quant.R to accept gtf annotation file

--- merge_salmon_quant/merge_salmon_quant.R
+++ merge_salmon_quant/merge_salmon_quant.R
@@ -7,19 +7,22 @@
 
 ############## Create CL parser
 library(optparse)
-parser = OptionParser(usage = "\n%prog [options] --salmon_dir <salmon_dir> --output_basename <output_basename>",
+parser = OptionParser(usage = "\n%prog [options] --salmon_dir <salmon_dir> --output_basename <output_basename> --gtf <gtf>",
                       description = "Program for merging salmon quant transcript-level output files for downstream RNAseq analysis",
                       prog="Rscript merge_salmon_quant.R")
 parser = add_option(object=parser, opt_str=c("--salmon_dir"), default=NULL, type="character",
-                    help="[REQUIRED] Path to directory containing salmon output files")
+                    help="[REQUIRED] Path to directory containing salmon output files.")
 parser = add_option(object=parser, opt_str=c("--output_basename"), default=NULL, type="character",
-                    help="[REQUIRED] Basename to prefix output files")
+                    help="[REQUIRED] Basename to prefix output files.")
+parser = add_option(object=parser, opt_str=c("--gtf"), default=NULL, type="character",
+                    help="[REQUIRED] Transcript annotation file in GTF format. Ideally should match the annotation file used to build the transcriptome used in quantification. Local paths and URLs are both valid.")
 ############## Parse command line
 argv = parse_args(parser)
 
 # parse cmdline params
 salmon_dir = argv$salmon_dir
 output_basename = argv$output_basename
+tx.gtf <- argv$gft
 
 # Options
 options(stringsAsFactors = F)
@@ -31,19 +34,18 @@ library(readr)
 library(GenomicFeatures)
 
 # Make tx-to-gene mapping
-gencode.gtf <- "ftp://ftp.ebi.ac.uk/pub/databases/gencode/Gencode_human/release_28/gencode.v28.annotation.gtf.gz"
-txdb <- makeTxDbFromGFF(file = gencode.gtf, format = "gtf",
-                        dataSource = "GencodeV28 Main Annotation File",
+txdb <- makeTxDbFromGFF(file = tx.gtf, format = "gtf",
+                        dataSource = "Transcript Annotation File",
                         organism = "Homo sapiens")
 txdb.keys <- keys(txdb, keytype = "TXNAME")
-tx2gene.v28 <- select(txdb, txdb.keys, "GENEID", "TXNAME")
+tx2gene <- select(txdb, txdb.keys, "GENEID", "TXNAME")
 
 
 # Import transcript level data from Salmon
 salmon.files <- list.files(".", pattern = "quant.sf", full.names = F)
 names(salmon.files) <- gsub("_quant.sf", "", salmon.files)
 txi.dte.tx <- tximport(files = salmon.files, type = "salmon", txIn = T, txOut = T,
-    countsFromAbundance = "no", tx2gene = tx2gene.v28,
+    countsFromAbundance = "no", tx2gene = tx2gene,
     ignoreAfterBar = T)
 
 # Rename transcripts
@@ -53,10 +55,10 @@ rownames(txi.dte.tx$abundance) <- tx.rownames
 rownames(txi.dte.tx$length) <- tx.rownames
 
 # Summarize to gene
-txi.gene.v28 <- summarizeToGene(txi.dte.tx, tx2gene = tx2gene.v28, countsFromAbundance = "no")
+txi.gene <- summarizeToGene(txi.dte.tx, tx2gene = tx2gene, countsFromAbundance = "no")
 
 # Save data
-dte_file = paste0(output_basename, "_salmon_dte_tx_data_gencode28.rds")
-gene_file = paste0(output_basename, "_salmon_gene_data_gencode28.rds")
+dte_file = paste0(output_basename, "_salmon_dte_tx_data.rds")
+gene_file = paste0(output_basename, "_salmon_dge_gene_data.rds")
 saveRDS(txi.dte.tx, dte_file)
-saveRDS(txi.gene.v28, gene_file)
+saveRDS(txi.gene, gene_file)
